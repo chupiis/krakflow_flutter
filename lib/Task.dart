64dd148ca29722bdf59a6a9cf97c1a20c9d17a6e
Marianna Chuprynina <chupiis0405@gmail.com>
2026-03-24 17:29:19 +0100

    lab_04 krakflow task list

---
+++ lib/Task.dart
@@ -0,0 +1,6 @@
+class Task {
+  final String title;
+  final String deadline;
+
+  Task({required this.title, required this.deadline});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,122 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+
+  List<Task> tasks = [
+    Task(title: "HTML/CSS", deadline: "dzisiaj"),
+    Task(title: "Python", deadline: "2 dni"),
+    Task(title: "Java", deadline: "w nastepnym tygodniu"),
+    Task(title: "C++", deadline: "jutro"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'KrakFlow',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(title: Text("KrakFlow")),
+        body: Center(
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Masz dzis ${tasks.length} zadania"),
+                ],
+              ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index){
+                    final task = tasks[index];
+                    return TaskCard(title: task.title,
+                        subtitle: task.deadline,
+                        icon: Icons.ice_skating);
+                  }
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text(
+                  "Dzisiejsze zadania",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ]
+              ),
+              TaskCard(title: tasks[0].title,
+                subtitle: "Hello",
+                icon: Icons.face,
+              ),
+              TaskCard(title: tasks[1].title,
+                subtitle: "Hello",
+                icon: Icons.face_2,
+              ),
+              TaskCard(title: tasks[2].title,
+                subtitle: "Hello",
+                icon: Icons.face_3,
+              ),
+              TaskCard(title: tasks[3].title,
+                subtitle: "Hello",
+                icon: Icons.face_4,
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'KrakFlow'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Task {
   final String title;
+  final String deadline;
+  final bool done;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Task({required this.title, required this.deadline, required this.done});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('KrakFlow'),
-            const Text('Organizacja studiow'),
-            const Text('Dzisiejsze zadanie'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
       ),
     );
   }
 }
+
+
+
+class MyTitle extends StatelessWidget{
+  const MyTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("KrakFlow");
+  }
+}
+
+
+
